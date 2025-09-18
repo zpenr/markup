f0f4821892818e44057ebabc6ff978d8d2eac874
zpenr <zpenr@bk.ru>
2025-09-18 10:17:27 +0800

    add second page and update changeColor function

--- scripts/script.js
+++ scripts/script.js
@@ -19,14 +19,66 @@ function changeHeight(){
     
 }
 
-function changeColor(){
+function changeColor(colorId){
     console.log("red")
     var table = document.getElementsByTagName("table")[0];
-    table.setAttribute("style","background-color: red;");
+    switch(colorId) {
+        case(0):{
+            table.setAttribute("style","background-color: red;");
+            break;
+        }
+        case(1):{
+            table.setAttribute("style","background-color: tomato;");
+            break;
+        }
+        case(2):{
+            table.setAttribute("style","background-color: darksalmon;");
+            break;
+        }
+        case(3):{
+            table.setAttribute("style","background-color: blue;");
+            break;
+        }
+        case(4):{
+            table.setAttribute("style","background-color: royalblue;");
+            break;
+        }
+        case(5):{
+            table.setAttribute("style","background-color: cornflowerblue;");
+            break;
+        }
+        case(6):{
+            table.setAttribute("style","background-color: green;");
+            break;
+        }
+        case(7):{
+            table.setAttribute("style","background-color: teal;");
+            break;
+        }
+        case(8):{
+            table.setAttribute("style","background-color: cadetblue;");
+            break;
+        }
+        case(9):{
+            table.setAttribute("style","background-color: white;");
+            break;
+        }
+    }
 }
 
 var inputWidth = document.getElementsByClassName("width")[0];
 var inputHeight = document.getElementsByClassName("height")[0];
-document.getElementById("red").addEventListener("mouseover",changeColor);
+document.getElementById("red").addEventListener("mouseover",() => changeColor(0));
+document.getElementById("tomato").addEventListener("mouseover",() => changeColor(1));
+document.getElementById("darksalmon").addEventListener("mouseover",() => changeColor(2));
+
+document.getElementById("blue").addEventListener("mouseover",() => changeColor(3));
+document.getElementById("royalblue").addEventListener("mouseover",() => changeColor(4));
+document.getElementById("cornflowerblue").addEventListener("mouseover",() => changeColor(5));
+
+document.getElementById("green").addEventListener("mouseover",() => changeColor(6));
+document.getElementById("teal").addEventListener("mouseover",() =>  changeColor(7));
+document.getElementById("cadetblue").addEventListener("mouseover",() => changeColor(8));
+
 inputWidth.addEventListener("input", changeWidth);
 inputHeight.addEventListener("input", changeHeight);
